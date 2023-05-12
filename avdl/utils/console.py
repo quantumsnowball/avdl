2239e7f4fe7faf1a489d8c1026c44c69ffd0a9c9
Quantum Snowball <quantum.snowball@gmail.com>
2023-05-13 02:36:57 +0800

    fix: readline arrow keys

--- avdl/utils/console.py
+++ avdl/utils/console.py
@@ -5,32 +5,32 @@ import click
 
 def require_user_input(message: str,
                        *args: Any,
-                       **kwargs: Any):
+                       **kwargs: Any) -> str:
     styled_message = click.style(message, fg='cyan')
-    return click.prompt(styled_message, *args, type=str, **kwargs)
+    return input(styled_message + ': ', *args, **kwargs)
 
 
 def print_warning(message: str,
                   *args: Any,
-                  **kwargs: Any):
+                  **kwargs: Any) -> None:
     return click.secho(message, *args, fg='yellow', **kwargs)
 
 
 def print_success(message: str,
                   *args: Any,
-                  **kwargs: Any):
+                  **kwargs: Any) -> None:
     return click.secho(message, *args, fg='green', **kwargs)
 
 
 def print_error(message: str,
                 *args: Any,
-                **kwargs: Any):
+                **kwargs: Any) -> None:
     return click.secho(message, *args, fg='red', **kwargs)
 
 
 def print_key_value(message: str,
                     value: Any,
                     *args: Any,
-                    **kwargs: Any):
+                    **kwargs: Any) -> None:
     styled_message = click.style(message, fg='blue')
     return click.echo(f'{styled_message}: {str(value)}', *args, **kwargs)
