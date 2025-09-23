ed7328908aca867abb6c5f24316b80cf3704b634
Quantum Snowball <quantum.snowball@gmail.com>
2025-09-23 19:50:22 +0800

    -o option for output file

--- avdl/partial/__init__.py
+++ avdl/partial/__init__.py
@@ -1,11 +1,16 @@
+from pathlib import Path
+
 import click
 from yarl import URL
 
 
 @click.command
 @click.argument('url', type=str, required=True)
+@click.option('-o', '--output', required=True, help='save as filename')
 def partial(
     url: str,
+    output: str,
 ) -> None:
     req_url = URL(url)
-    print(f'Will download {req_url}')
+    output_file = Path(output)
+    print(f'Will download {req_url} and save as {output_file}')
