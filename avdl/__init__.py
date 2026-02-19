ab25a1f15f717aa46c96952cc774926e22a0ae4f
Quantum Snowball <quantum.snowball@gmail.com>
2026-02-20 05:06:59 +0800

    refactor, app is always in __init__.py, sub-command is a module or another package/__init__.py

--- avdl/__init__.py
+++ avdl/__init__.py
@@ -1,2 +1,12 @@
 # enable advance key press, e.g. arrow keys
 import readline
+
+import typer
+
+from avdl.m3u8 import app as m3u8
+from avdl.partial import app as partial
+
+app = typer.Typer(no_args_is_help=True)
+
+app.add_typer(m3u8)
+app.add_typer(partial)
